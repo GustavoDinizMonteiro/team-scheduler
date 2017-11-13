6bc2abfe27161452b221c3564d1bddef729d6de3
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2017-11-13 11:12:40 -0300

    Added configuration for requests and added archive for mocks.

---
+++ metrics/config.js
@@ -0,0 +1,35 @@
+const config = {
+	url: 'http://localhost:3000',
+	routes: [
+		{ 
+			url: '/team',
+			operations: ['post']
+    },
+    {
+      url: '/team/members',
+      operations: ['get']
+    },
+    {
+      url: '/team/pending-members',
+      operations: ['get', 'patch']
+    },
+    {
+      url: '/team/tag',
+      operations: ['post', 'get', 'patch']
+    },
+    {
+      url: '/user/hours',
+      operations: ['get', 'patch']
+    },
+    {
+      url: '/login',
+      operations: ['post']
+    },
+    {
+      url: '/signup',
+      operations: ['post']
+    }
+	]
+};
+
+module.exports = config;
