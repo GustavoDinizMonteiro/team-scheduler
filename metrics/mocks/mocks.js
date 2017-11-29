782f6a1802cbec571dcce16e54fef24b3b22abb4
Gustavo Diniz Monteiro 115111490 <gustavodinizmonteiro>
2017-11-29 10:59:34 -0300

    Added metrics of local machine and database time.

--- metrics/mocks/mocks.js
+++ metrics/mocks/mocks.js
@@ -14,14 +14,14 @@ module.exports = {
     },
     "/pending-members": {
         userId: "5a1e1c5e094349001abea366",
-        team: "5a1dbead2fc49c0018f9f0b5",
+        team: "5a1eb816a62ec05a75e86fa9",
         action: "accept"
     },
     "/signup": {
         name: faker.internet.userName(),
         email: faker.internet.email(),
         password: faker.internet.password(),
-        team: "5a1dbead2fc49c0018f9f0b5"
+        team: "5a1eb816a62ec05a75e86fa9"
     }
 }
 
